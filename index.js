83772a9f7a55f54fea7ea4456cfb902b3c9641f1
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-10-04 14:52:08 +0300

    enhance index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import compression from "compression";
 import redis from "redis";
-import { getRedisPosts } from "./redis.js";
 import expressCache from "cache-express";
 
 const app = express();
@@ -11,6 +10,44 @@ const PORT = process.env.PORT || 4000;
 export const redisClient = redis.createClient({
   url: "redis://default:mgpnoCLgHTDOpZflxfHqsabhihYriWjE@junction.proxy.rlwy.net:46125",
 });
+
+export async function getRedisPosts(limit = 10, offset = 0) {
+  try {
+    let results = [];
+    const max = limit + offset - 1;
+    const posts = await redisClient.zRange("posts", offset, max, {
+      REV: true,
+    });
+
+    posts.map((post) => {
+      const data = JSON.parse(post);
+      results.push({
+        id: data.id,
+        title: data.title,
+        date: data.date,
+        url: data.url,
+        main_image: data.main_image,
+        images: data.images,
+        drive_img_id: data.drive_img_id,
+        status: data.status,
+        is_related_to_yemen: data.is_related_to_yemen,
+        states: data.states,
+        countries: data.countries,
+        categories: data.categories,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+        host: data.host,
+        is_duplicated: data.is_duplicated,
+        test: data.test,
+        summary: data.summary,
+        keywords: data.keywords,
+      });
+    });
+    return results;
+  } catch (err) {
+    return [];
+  }
+}
 
 (async () => {
   redisClient.on("error", (err) => console.log("Redis Client Error", err));
