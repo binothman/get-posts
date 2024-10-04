c00faf5fb655bba4bbccc0cb23e566fd26ede206
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-10-04 07:25:03 +0300

    add redis

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import compression from "compression";
+import redis from "redis";
+import { getRedisPosts } from "./redis.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+export const redisClient = redis.createClient({
+  url: "redis://default:mgpnoCLgHTDOpZflxfHqsabhihYriWjE@junction.proxy.rlwy.net:46125",
+});
 
 app.use(compression(), express.json({ limit: "50mb" }));
 
