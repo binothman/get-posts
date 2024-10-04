ed31228e0610f436313aef6da45529f3fad50049
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-10-04 07:28:33 +0300

    fix: 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import compression from "compression";
 import redis from "redis";
-// import { getRedisPosts } from "./redis.js";
+import { getRedisPosts } from "./redis.js";
 
 const app = express();
 
@@ -11,11 +11,29 @@ export const redisClient = redis.createClient({
   url: "redis://default:mgpnoCLgHTDOpZflxfHqsabhihYriWjE@junction.proxy.rlwy.net:46125",
 });
 
+(async () => {
+  redisClient.on("error", (err) => console.log("Redis Client Error", err));
+  redisClient.on("ready", () => {
+    console.log("Redis Client Connected!");
+  });
+
+  await redisClient.connect();
+})();
+
 app.use(compression(), express.json({ limit: "50mb" }));
 
 app.use("/redis", async function (req, res) {
+  const { limit, offset } = req.query;
+
+  const data = await Promise.all([
+    getRedisPosts(parseInt(limit) || undefined, parseInt(offset) || undefined),
+    redisClient.zCount("posts", "-inf", "+inf"),
+  ]);
+
   res.send({
-    success: 0,
+    count: data[0].length,
+    total: data[1],
+    posts: data[0],
   });
 });
 
