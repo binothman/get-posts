99e99f0dec3f42ccab8f7ba8b0ed2ee3b7a2a9cf
Mohamed Bin Othman <binothman.mo@gmail.com>
2024-10-04 14:42:30 +0300

    enhance get posts

--- redis.js
+++ redis.js
@@ -9,7 +9,28 @@ export async function getRedisPosts(limit = 10, offset = 0) {
     });
 
     posts.map((post) => {
-      results.push(JSON.parse(post));
+      const data = JSON.parse(post);
+      results.push({
+        id: data.id,
+        title: data.title,
+        date: data.date,
+        url: data.url,
+        main_image: data.main_image,
+        images: data.images,
+        drive_img_id: data.drive_img_id,
+        status: data.status,
+        is_related_to_yemen: data.is_related_to_yemen,
+        states: data.states,
+        countries: data.countries,
+        categories: data.categories,
+        createdAt: data.createdAt,
+        updatedAt: data.updatedAt,
+        host: data.host,
+        is_duplicated: data.is_duplicated,
+        test: data.test,
+        summary: data.summary,
+        keywords: data.keywords,
+      });
     });
     return results;
   } catch (err) {
